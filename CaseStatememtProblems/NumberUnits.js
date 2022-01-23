753c2480b5a9846101729125e9aa5526cf3242c4
“Enter <anirudhanand984@gmail.com>
2022-01-23 13:55:06 +0530

    [Anirudh] add : Case Statememt Problems

---
+++ CaseStatememtProblems/NumberUnits.js
@@ -0,0 +1,33 @@
+var number=10;
+switch(number){
+    case 1:
+        console.log("One");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundread");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten Thousand");
+        break;
+    case 100000:
+        console.log("Lakh");
+        break;
+    case 1000000:
+        console.log("Ten Lakh");
+        break;
+    case 10000000:
+        console.log("Crore");
+        break;
+    case 100000000:
+        console.log("Ten Crore");
+        break;
+    default:
+        console.log("Invalid Choice");
+        break;
+}
